954d83385f5b9dbfb5b3dcef9e7e99af4587414d
Pr0grammerSimon <szymon@krzywda.eu>
2024-06-25 20:42:39 +0200

    YEY Update

---
+++ nav.js
@@ -0,0 +1,15 @@
+
+
+function nav(){
+    let nav = document.getElementById("nav-div");
+    if (nav.style.display == "none"){
+        nav.style.display = "flex";
+
+    }
+    else{
+        nav.style.display = "none";
+    }
+
+
+}
+
