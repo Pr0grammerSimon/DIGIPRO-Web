902de3616dd1b1f02204cd27a5b91460aff03f81
Pr0grammerSimon <szymon@krzywda.eu>
2024-06-26 11:13:50 +0200

    Update!

---
+++ carousel.js
@@ -0,0 +1,22 @@
+let next = document.getElementById("next");
+let prev =  document.getElementById("prev");
+next.addEventListener("click", () => {
+    let opinie = document.getElementById("opinie");
+
+    opinie.scrollBy({left: opinie.scrollWidth/4, behavior:"smooth"})
+
+    prev.style.display = opinie.scrollLeft < 0 ? "none" : "block";
+    next.style.display = opinie.scrollLeft >= ((opinie.scrollWidth) - (opinie.scrollWidth/2)) ? "none" : "block";
+});
+
+prev.addEventListener("click", () => {
+    let opinie = document.getElementById("opinie");
+
+    opinie.scrollBy({left: -opinie.scrollWidth/4, behavior:"smooth"})
+
+    prev.style.display = opinie.scrollLeft <= opinie.scrollWidth/4 ? "none" : "block";
+    next.style.display = opinie.scrollLeft >= ((opinie.scrollWidth) - (opinie.scrollWidth/2)) ? "none" : "block";
+    
+
+ 
+});
