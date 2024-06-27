44a5c26798bd9e81db8bdf8a9fe7d21bed1c3aba
Pr0grammerSimon <szymon@krzywda.eu>
2024-06-27 11:36:13 +0200

    Porządek

--- wiadomosc.php
+++ wiadomosc.php
@@ -3,12 +3,14 @@
 
 if (isset($_POST["submit"])){
 
-    $to = "somone@gmail.com";
+    $to = "szymon@krzywda.eu";
 
     $headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
     $headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
 
     $headers.= 'From: =?UTF-8?B?'. base64_encode($_POST["mail-in"]). '?=';
+    //echo $headers;
+    //$from = "=?UTF-8?B?".base64_encode("no-reply@digipro.pl")."?=";
 
     $subject = '=?UTF-8?B?' . base64_encode($_POST["temat"]) . '?=';
 
